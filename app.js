970e148bedffd0384751ea518433539ce425e64a
randika96 <rpjayasekara.16@cse.mrt.ac.lk>
2018-03-26 23:11:02 +0530

    First commit of SMS

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/",function (req,res) {
+    res.render("index");
+})
+
+
+app.listen(3000,function () {
+    console.log("SMS server has started!")
+})
